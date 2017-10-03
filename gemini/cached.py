bf69f834b8fc55599b13d4dae2a7ec9a9f1f56b6
mtusman <m.t.usman@hotmail.com>
2017-10-03 19:24:18 +0100

    Created singleton and cached metaclasses

---
+++ gemini/cached.py
@@ -0,0 +1,20 @@
+# cached.py
+# Mohammad Usman
+#
+# A metaclass that creates catched instances.
+
+import weakref
+
+
+class Cached(type):
+    def __init__(self, *args, **kwargs):
+        self.__cache = weakref.WeakValueDictionary()
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, *args):
+        if args in self.__cache:
+            return self.__cache[args]
+        else:
+            obj = super().__call__(*args)
+            self.__cache[args] = obj
+            return obj
